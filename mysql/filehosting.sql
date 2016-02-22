7d25e5eba8f4197d80d9603eb6267a00c54fea0b
adwisatya <a.dwisaty4@yahoo.com>
2016-02-22 10:06:50 +0700

    Update tabel untuk comment Signed-off-by: adwisatya <a.dwisaty4@yahoo.com>

--- mysql/filehosting.sql
+++ mysql/filehosting.sql
@@ -1,110 +1,183 @@
--- MySQL dump 10.13  Distrib 5.6.24, for Win32 (x86)
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: filehosting
--- ------------------------------------------------------
--- Server version	5.6.24
+-- Host: 127.0.0.1
+-- Generation Time: Feb 22, 2016 at 04:06 AM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `filehosting`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `com_id` int(4) NOT NULL,
+  `com_name` varchar(100) NOT NULL,
+  `com_email` varchar(100) NOT NULL,
+  `com_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `com_dis` text NOT NULL,
+  `pid_fk` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`com_id`, `com_name`, `com_email`, `com_date`, `com_dis`, `pid_fk`) VALUES
+(1, 'untuk aryya', 'dsadsa', '2016-02-21 17:00:00', 'dasdasdas', 2);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `file`
 --
 
-DROP TABLE IF EXISTS `file`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `file` (
-  `fileid` int(5) NOT NULL AUTO_INCREMENT,
+  `fileid` int(5) NOT NULL,
   `filename` text NOT NULL,
   `path` text NOT NULL,
   `status` int(1) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  PRIMARY KEY (`fileid`)
-) ENGINE=InnoDB AUTO_INCREMENT=11114 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `username` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `file`
 --
 
-LOCK TABLES `file` WRITE;
-/*!40000 ALTER TABLE `file` DISABLE KEYS */;
-INSERT INTO `file` VALUES (1,'20122013 Notulensi kumpul CAPTCHA.pdf','2f28ad79372067db8d2e6cbbef08e6de.pdf',1,'hayyuhanifah'),(11111,'Panduan Membuat Web.pdf','23232312312321312312312.pdf',1,'aryya'),(11112,'Fiu siu.exe','27827872.exe',1,'aryya'),(11113,'Desert.jpg','87d07f90f10c40ed8075f45c042fe6f6.jpg',1,'hayyuhanifah');
-/*!40000 ALTER TABLE `file` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `file` (`fileid`, `filename`, `path`, `status`, `username`) VALUES
+(1, '20122013 Notulensi kumpul CAPTCHA.pdf', '2f28ad79372067db8d2e6cbbef08e6de.pdf', 1, 'hayyuhanifah'),
+(11111, 'Panduan Membuat Web.pdf', '23232312312321312312312.pdf', 1, 'aryya'),
+(11112, 'Fiu siu.exe', '27827872.exe', 1, 'aryya'),
+(11113, 'Desert.jpg', '87d07f90f10c40ed8075f45c042fe6f6.jpg', 1, 'hayyuhanifah');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `post`
 --
 
-DROP TABLE IF EXISTS `post`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `post` (
-  `pid` int(5) NOT NULL AUTO_INCREMENT,
+  `pid` int(5) NOT NULL,
   `title` varchar(100) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `content` text NOT NULL,
   `isDeleted` tinyint(2) DEFAULT '0',
-  `username` varchar(20) NOT NULL,
-  PRIMARY KEY (`pid`),
-  KEY `fk_username` (`username`),
-  CONSTRAINT `fk_username` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `username` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `post`
 --
 
-LOCK TABLES `post` WRITE;
-/*!40000 ALTER TABLE `post` DISABLE KEYS */;
-INSERT INTO `post` VALUES (1,'Test','2016-02-20 16:26:42','uwouwo',0,'hayyuhanifah'),(2,'hahha','2016-02-20 16:58:28','Lorem ipsum',0,'hayyuhanifah');
-/*!40000 ALTER TABLE `post` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `post` (`pid`, `title`, `date`, `content`, `isDeleted`, `username`) VALUES
+(1, 'Test', '2016-02-20 16:26:42', 'uwouwo', 0, 'hayyuhanifah'),
+(2, 'hahha', '2016-02-20 16:58:28', 'Lorem ipsum', 0, 'hayyuhanifah'),
+(3, 'dsa', '2016-02-22 03:03:19', 'dsadsa', 0, 'aryya');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `username` varchar(20) NOT NULL,
   `password` varchar(32) NOT NULL,
   `email` text NOT NULL,
-  `active` int(1) NOT NULL,
-  PRIMARY KEY (`username`)
+  `active` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('aryya','84c29c7a16116c6e549fcafdc4a8fcf1','a.dwisatya@yahoo.com',1),('aryyaedwisatya','7c1cadb6887373dacb595c47166bfbd9','aryya@gdoogle.com',0),('dwisatya','afe22da9ddbcb15d6ae186a56fd280fa','dwisatya',0),('hayyuhanifah','5f4dcc3b5aa765d61d8327deb882cf99','hayyuhanifah52@gmail.com',1);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `user` (`username`, `password`, `email`, `active`) VALUES
+('aryya', '84c29c7a16116c6e549fcafdc4a8fcf1', 'a.dwisatya@yahoo.com', 1),
+('aryyaedwisatya', '7c1cadb6887373dacb595c47166bfbd9', 'aryya@gdoogle.com', 0),
+('dwisatya', 'afe22da9ddbcb15d6ae186a56fd280fa', 'dwisatya', 0),
+('hayyuhanifah', '5f4dcc3b5aa765d61d8327deb882cf99', 'hayyuhanifah52@gmail.com', 1);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`com_id`),
+  ADD KEY `pid_fk` (`pid_fk`);
+
+--
+-- Indexes for table `file`
+--
+ALTER TABLE `file`
+  ADD PRIMARY KEY (`fileid`);
+
+--
+-- Indexes for table `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`pid`),
+  ADD KEY `fk_username` (`username`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `com_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `file`
+--
+ALTER TABLE `file`
+  MODIFY `fileid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11114;
+--
+-- AUTO_INCREMENT for table `post`
+--
+ALTER TABLE `post`
+  MODIFY `pid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`pid_fk`) REFERENCES `post` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `fk_username` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-02-21  0:02:59
